3dbf940a1a77957a2d9aaf3056b532c72630cf41
heron <peng.liu@nkn.org>
2019-08-30 16:06:06 +0800

    wtf
    Signed-off-by: heron <peng.liu@nkn.org>

--- ios/RNOS.h
+++ ios/RNOS.h
@@ -8,7 +8,11 @@
 
 #import <Foundation/Foundation.h>
 #import <SystemConfiguration/SCNetworkReachability.h>
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
 
